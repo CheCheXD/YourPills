056e4e97f8cf5902068d996eb91348fafe0c3044
Moura <guilherme4gamerxd@gmail.com>
2022-12-07 16:12:27 +0000

    criei o layout dos comprimidos e das receitas e meti os botões do HomeActivity a funcionar

---
+++ app/src/main/java/com/example/yourpills/ComprimidosActivity.java
@@ -0,0 +1,14 @@
+package com.example.yourpills;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComprimidosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comprimidos);
+    }
+}
--- app/src/main/java/com/example/yourpills/HomeActivity.java
+++ app/src/main/java/com/example/yourpills/HomeActivity.java
@@ -1,26 +1,65 @@
 package com.example.yourpills;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.ContentValues;
-import android.database.Cursor;
+
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.EditText;
-import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
     private DB DB;
-    private EditText editText;
     private CalendarView calendarView;
     private String selectedDate;
     private SQLiteDatabase sqLiteDatabase;
+    private Button calendario, comprimido, receita, rotina;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        calendarView = findViewById(R.id.calendarView);
+        calendario = (Button) findViewById(R.id.calendario);
+        comprimido = (Button) findViewById(R.id.comprimido);
+        receita = (Button) findViewById(R.id.receita);
+        rotina = (Button) findViewById(R.id.rotina);
+
+
+        calendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        comprimido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ComprimidosActivity.class );
+                startActivity(intent);
+            }
+        });
+
+        receita.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ReicetasActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        rotina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RotinaActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
